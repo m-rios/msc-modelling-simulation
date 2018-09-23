19590fee28775640be0054b53b42746c4b7bb359
Mario Ríos Muñoz <mrm994@gmail.com>
2018-09-23 11:47:27 +0200

    Packages refactoring
    Cleaned up the packages' structure so the imports make more sense and are more monolithic.

--- integrators/selector.py
+++ integrators/selector.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from model.universe import Universe
-
+import numpy as np
 
 class Selector(ABC):
     # Abstract selector class. Selector classes implement a way of choosing the stars that will be use to compute
@@ -15,3 +15,12 @@ class Selector(ABC):
         :return: A new universe which only contains the stars that will attract star_idx
         """
         raise NotImplemented
+
+
+class AllSelector(Selector):
+
+    def select(self, star_idx: int, universe: Universe) -> Universe:
+        sub_universe = Universe()
+        sub_universe.n_stars = universe.n_stars - 1
+        sub_universe.stars = np.delete(universe.stars, star_idx)
+        return sub_universe
