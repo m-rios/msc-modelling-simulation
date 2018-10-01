1bdd9e3b71138b66c75525a3f40e878690d11865
GeneralZYQ <zhangyuanqing.generalzyq@gmail.com>
2018-10-01 23:08:40 +0200

    leapfrog

--- integrators/leapfrog.py
+++ integrators/leapfrog.py
@@ -3,11 +3,14 @@ import sys
 sys.path.append('/Users/zhangyuanqing/Documents/Groningen/simulationAndModelling/msc-modelling-simulation/model') 
 import universe
 
-# def leapfrogCalculating(star, timeStep):
-    
-#     halfLocation = star.pos + timeStep * star.pos / 2
-#     veloN2 = star.vel  + timeStep * star.acc * halfLocation
-#     locationN2 = halfLocation + timeStep * veloN2 / 2
+def leapfrogCalculating(calculatedUniverse, timeStep):
 
-#     star.vel = veloN2
-#     star.pos = locationN2
+    for star in calculatedUniverse.n_stars:
+        starhalfstep = star.pos + (timeStep * star.vel) / 2 # 13a
+        star.pos = starhalfstep
+    for star in calculatedUniverse.n_stars:
+        star.acc = calculatedUniverse.getAccelerationOnStar(star) # new acceleration
+        
+    for star in calculatedUniverse.n_stars:
+        star.vel = star.vel + timeStep * star.acc #get new vel (13b)
+        star.pos = star.pos + timeStep * star.vel / 2
