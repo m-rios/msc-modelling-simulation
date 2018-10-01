1448fbc9f94c8b39053f09ba75f33b0281823fb5
GeneralZYQ <zhangyuanqing.generalzyq@gmail.com>
2018-10-01 22:44:13 +0200

    bad implementation!

--- integrators/leapfrog.py
+++ integrators/leapfrog.py
@@ -3,11 +3,11 @@ import sys
 sys.path.append('/Users/zhangyuanqing/Documents/Groningen/simulationAndModelling/msc-modelling-simulation/model') 
 import universe
 
-def leapfrogCalculating(star, timeStep):
+# def leapfrogCalculating(star, timeStep):
     
-    halfLocation = star.pos + timeStep * star.pos / 2
-    veloN2 = star.vel  + timeStep * star.acc * halfLocation
-    locationN2 = halfLocation + timeStep * veloN2 / 2
+#     halfLocation = star.pos + timeStep * star.pos / 2
+#     veloN2 = star.vel  + timeStep * star.acc * halfLocation
+#     locationN2 = halfLocation + timeStep * veloN2 / 2
 
-    star.vel = veloN2
-    star.pos = locationN2
+#     star.vel = veloN2
+#     star.pos = locationN2
